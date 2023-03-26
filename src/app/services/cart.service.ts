327435c22cc6d888e9afb57ea91aea5875034b2c
mostafa-soliman <m.fadaly52@gmail.com>
2023-03-26 23:55:55 +0200

    market

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,52 @@
+import { Iitem } from './../interface/iitem';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  items:Iitem[]
+
+  constructor() {
+   let carts= this.getFromStorage()
+    if (carts) {
+      this.items=carts
+    }else
+    {
+    this.items=[]
+    }
+    }
+
+    addToCart(item:Iitem){
+
+    this.items.push(item)
+    this.addCartToStorage()
+    }
+    // get data from home to cart
+    getCartItems():Iitem[]{
+      return this.items
+    }
+
+    // addToStorage(item:Iitem){
+    //   // localStorage.setItem('items',item)
+    // }
+        //
+    addCartToStorage(){
+      localStorage.setItem('cart',JSON.stringify(this.items))
+    }
+    getFromStorage(){
+      let cartItems=JSON.parse(localStorage.getItem('cart')!);
+      return cartItems;
+    }
+    removeItemFromStorage(item:Iitem){
+          this.items=this.items.filter(i=> i.id!==item.id)
+
+
+    }
+    clearStorage(){
+      this.items=[]
+      localStorage.setItem('cart',JSON.stringify(this.items))
+    }
+
+}
+
