b5eea91d91cb0bb543583486699aa02204a2e4fa
mostafa-soliman <m.fadaly52@gmail.com>
2023-03-31 22:06:45 +0200

    market

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,52 +1,92 @@
 import { Iitem } from './../interface/iitem';
 import { Injectable } from '@angular/core';
+import { ItemsService } from './items.service';
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
-  items:Iitem[]
+  items: Iitem[];
+  x: number = 1;
 
-  constructor() {
-   let carts= this.getFromStorage()
+  constructor(private itemsService: ItemsService) {
+    let carts = this.getFromStorage();
     if (carts) {
-      this.items=carts
-    }else
-    {
-    this.items=[]
+      this.items = carts;
+    } else {
+      this.items = [];
     }
-    }
+  }
 
-    addToCart(item:Iitem){
+  addToCart(item: Iitem) {
+    this.items = this.items.map((element) => {
+      if (item.id === element.id) {
+        element.quantity++;
+      }
+      return element;
+    });
+    this.items.push(item);
 
-    this.items.push(item)
-    this.addCartToStorage()
-    }
-    // get data from home to cart
-    getCartItems():Iitem[]{
-      return this.items
-    }
+    this.addCartToStorage();
+  }
+  // get data from home to cart
+  getCartItems(): Iitem[] {
+    return this.items;
+  }
 
-    // addToStorage(item:Iitem){
-    //   // localStorage.setItem('items',item)
-    // }
+  // addToStorage(item:Iitem){
+  //   // localStorage.setItem('items',item)
+  // }
 
-    addCartToStorage(){
-      localStorage.setItem('cart',JSON.stringify(this.items))
-    }
-    getFromStorage(){
-      let cartItems=JSON.parse(localStorage.getItem('cart')!);
-      return cartItems;
-    }
-    removeItemFromStorage(item:Iitem){
-          this.items=this.items.filter(i=> i.id!==item.id)
+  addCartToStorage() {
+    localStorage.setItem('cart', JSON.stringify(this.items));
+  }
+  getFromStorage() {
+    let cartItems = JSON.parse(localStorage.getItem('cart')!);
+    return cartItems;
+  }
+  removeItemFromStorage(item: Iitem) {
+    this.items = this.items.filter((i) => i.id !== item.id);
+  }
+  clearStorage() {
 
+    this.items = [];
+    localStorage.setItem('cart', JSON.stringify(this.items));
+  }
+  // item.quantity * item.price
 
-    }
-    clearStorage(){
-      this.items=[]
-      localStorage.setItem('cart',JSON.stringify(this.items))
-    }
+  incrementQun(item: Iitem) {
+    // localStorage.getItem(JSON.stringify(item.quantity=))
+    // console.log('dddd' + localStorage.getItem(JSON.stringify(item.quantity)));
+    // return item.quantity++;
+    // item.quantity++;
+    //  this.items.pop(item.quantity)
+    // this.x=this.items.find(()=>item.quantity)
+    // console.log(++item.quantity)
+    // item.quantity++
 
+    this.items = this.items.map((element) => {
+      if (item.id === element.id) {
+        element.quantity++;
+      }
+      return element;
+    });
+    this.addCartToStorage();
+  }
+
+  decrementQun(item: Iitem) {
+    this.items = this.items
+      .map((element) => {
+        if (item.id === element.id) {
+          element.quantity--;
+        }
+        if(element.quantity===0){
+          this.itemsService.isInCart(element);
+        }
+        return element;
+      })
+      .filter((e) => e.quantity >= 1); //e delete any quantity =0
+    this.addCartToStorage();
+  }
 }
-
